acdd8524af7df70c52d80407c380d03936e7be75
Bahanack-GY <georgesyvan12@gmail.com>
2026-02-14 10:44:25 +0100

    updated api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-// export const api = axios.create({
-//     baseURL: 'https://api.sniperbusinessebook.online/', 
-// });
-
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/', 
+    baseURL: 'https://api.sniperbusinessebook.online/', 
 });
+
+// export const api = axios.create({
+//     baseURL: 'http://localhost:3000/', 
+// });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
